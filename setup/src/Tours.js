31d36869193e685fff0b47dfdc529baa5a4f3a5f
jihoon <akdlsz21@gmail.com>
2021-09-08 23:31:40 +0900

    comment of personal thoughts of what is happening under the hood when props are send to other components

--- setup/src/Tours.js
+++ setup/src/Tours.js
@@ -11,6 +11,10 @@ const Tours = ({ tours, removeTour }) => {
 			<div>
 				{tours.map((tour) => {
 					return <Tour key={tour.id} {...tour} removeTour={removeTour} />;
+					//What is doing under the hood is const props = {...tour}. So, in Tour component, function parameter instant destructoring is able.
+					// Since passing arguments in a function is the same as function foobar(x){} , foobar('string') === const x = 'string'
+					// in the same sense, function Tour(props){...}, props = {...tour}
+					// So in Tour component parameter, an implicit instant destructoring is available. These are just what I think, require further study.
 				})}
 			</div>
 		</section>
